8a1db60ec72854fab03cc3239f64e4f28ab5d56f
a-hsu <a.hsu873@gmail.com>
2021-08-10 13:54:49 -0400

    REACTO - sumPrimes

---
+++ src/Euler/SummationOfPrimes/index.js
@@ -0,0 +1,5 @@
+function summationOfPrimes(max) {
+  return 0
+}
+
+module.exports = summationOfPrimes
--- src/Euler/SummationOfPrimes/sumPrimes.test.js
+++ src/Euler/SummationOfPrimes/sumPrimes.test.js
@@ -1,12 +1,12 @@
 const summationOfPrimes = require("./index.js")
 
-test("How many primes are there below 30?", () => {
+test("What is the sum of primes below 30?", () => {
     expect(summationOfPrimes(30)).toBe(129)
 })
-test("How many primes are there below 100?", () => {
+test("What is the sum of primes below 100?", () => {
     expect(summationOfPrimes(100)).toBe(1060)
 })
-test("How many primes are there below 1000000?", () => {
+test("What is the sum of primes below 1000000?", () => {
     expect(summationOfPrimes(2000000)).toBe(142913828922)
 })
 
