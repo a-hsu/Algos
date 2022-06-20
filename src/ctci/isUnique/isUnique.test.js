05f07b5ee98f80f1e7b7783d4bc097de493a70f5
a-hsu <a.hsu873@gmail.com>
2022-06-20 15:29:33 -0400

    update

--- src/ctci/isUnique/isUnique.test.js
+++ src/ctci/isUnique/isUnique.test.js
@@ -5,10 +5,11 @@ test("returns FALSE if string does not have unique characters ", () => {
     expect(isUnique("112233")).toBe(false)
     expect(isUnique("abcABC")).toBe(false)
 })
-
 test("returns TRUE if string has unique characters ", () => {
     expect(isUnique("computer")).toBe(true)
     expect(isUnique("Olympics")).toBe(true)
     expect(isUnique("tacos")).toBe(true)
     expect(isUnique("foil")).toBe(true)
 })
+
+
